c169d40452777ad4b2fc19dfa03796234718a6e1
wylazy <ipjmcp@gmail.com>
2018-07-31 17:45:11 +0800

    remove mutex

--- main.c
+++ main.c
@@ -19,7 +19,6 @@ static char server_err[ANET_ERR_LEN];
 
 
 typedef struct EchoClient {
-
     char ip[IP_STR_LEN];
     int port;
 
@@ -27,19 +26,15 @@ typedef struct EchoClient {
     int pos; /*The first filled position in buf*/
     int size; /*received data in buffer*/
 
-    pthread_mutex_t mutex;
-
 } EchoClient;
 
 void init_client(EchoClient * client) {
     client->port = 0;
     client->pos = 0;
     client->size = 0;
-    pthread_mutex_init(&client->mutex, NULL);
 }
 
 void destroy_client(EchoClient * client) {
-    pthread_mutex_destroy(&client->mutex);
 }
 
 void close_client(aeEventLoop * loop, EchoClient * client, int fd) {
@@ -52,17 +47,15 @@ void close_client(aeEventLoop * loop, EchoClient * client, int fd) {
  * Event loop has found the fd for write is avalibable
  * */
 void onWritePrepared(aeEventLoop * loop, int fd, void * privdata, int mask) {
-
     EchoClient * client = (EchoClient *)privdata;
-    pthread_mutex_lock(&client->mutex);
     int nwrite = write(fd, client->buf+client->pos, client->size);
     if (nwrite <= 0) {
         close_client(loop, client, fd);
+        return;
     }
     printf("SEND->(%s,%d): %-*s\n", client->ip, client->port, client->size, client->buf + client->pos);
     client->pos += nwrite;
     client->size -= nwrite;
-    pthread_mutex_unlock(&client->mutex);
 
     if (client->size == 0) {
         aeDeleteFileEvent(loop, fd, AE_WRITABLE);
@@ -74,25 +67,16 @@ void onWritePrepared(aeEventLoop * loop, int fd, void * privdata, int mask) {
  * when read data from fd to client->buf, then add aeCreateFileEvent for write
  */
 void onReadPrepared(aeEventLoop * loop, int fd, void * privdata, int mask) {
-
     EchoClient * client = (EchoClient *)privdata;
-
-    pthread_mutex_lock(&client->mutex);
-
     int nread = read(fd, client->buf + client->pos, BUF_SIZE-client->pos);
-
     if (nread <= 0) {
         close_client(loop, client, fd);
         return;
     }
 
     client->size += nread;
-
     printf("RECV<-(%s,%d): %-*s\n", client->ip, client->port, client->size, client->buf+client->pos);
-    pthread_mutex_unlock(&client->mutex);
-
     aeCreateFileEvent(loop, fd, AE_WRITABLE, onWritePrepared, client);
-
 }
 
 /**
@@ -100,8 +84,7 @@ void onReadPrepared(aeEventLoop * loop, int fd, void * privdata, int mask) {
  */
 void acceptTcpHandler(aeEventLoop * loop, int fd, void *privdata, int mask) {
     int cfd;
-
-    EchoClient * client = malloc(sizeof(EchoClient));
+    EchoClient * client = (EchoClient*)malloc(sizeof(EchoClient));
 
     if (client == NULL) {
         fprintf(stderr, "malloc failed!");
@@ -109,7 +92,6 @@ void acceptTcpHandler(aeEventLoop * loop, int fd, void *privdata, int mask) {
     }
 
     init_client(client);
-
     /*accept the new client*/
     cfd = anetTcpAccept(server_err, fd, client->ip, sizeof(client->ip), &client->port);
     if (cfd == ANET_ERR) {
@@ -121,29 +103,29 @@ void acceptTcpHandler(aeEventLoop * loop, int fd, void *privdata, int mask) {
 
     /*add the new cfd to epoll*/
     if (aeCreateFileEvent(loop, cfd, AE_READABLE, onReadPrepared, client) == AE_ERR) {
-         close(fd);
-         destroy_client(client);
-         free(client);
-         client = NULL;
+        close(fd);
+        destroy_client(client);
+        free(client);
+        client = NULL;
     }
 }
 
 int main() {
-   int fd = 0;
-   aeEventLoop * loop = NULL;
+    int fd = 0;
+    int port = 10372;
+    aeEventLoop * loop = NULL;
+    loop = aeCreateEventLoop(MAX_FD);
+    fd = anetTcpServer(server_err, port, NULL, BACK_LOG);
 
-   loop = aeCreateEventLoop(MAX_FD);
-   fd = anetTcpServer(server_err, 10372, NULL, BACK_LOG);
-
-   if (fd > 0) {
-       if (aeCreateFileEvent(loop, fd, AE_READABLE, acceptTcpHandler, NULL) == AE_ERR) {
+    if (fd > 0) {
+        if (aeCreateFileEvent(loop, fd, AE_READABLE, acceptTcpHandler, NULL) == AE_ERR) {
             fprintf(stderr, "CreateFileEvent error!\n");
             return EXIT_FAILURE;
-       }
-   } else {
-       fprintf(stderr, "anetTcpServer error");
-       return EXIT_FAILURE;
-   }
+        }
+    } else {
+        fprintf(stderr, "anetTcpServer error");
+        return EXIT_FAILURE;
+    }
 
     aeMain(loop);
     aeDeleteEventLoop(loop);
